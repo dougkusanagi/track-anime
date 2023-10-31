b69b6b74609411bce10c3e30efb219768cdb4fdc
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-10-31 18:22:00 -0300

    Update SavedAnimeController.php

--- app/Http/Controllers/SavedAnimeController.php
+++ app/Http/Controllers/SavedAnimeController.php
@@ -21,7 +21,7 @@ class SavedAnimeController
         $anime = JikanMoeAnimesService::findByMalId($request->mal_id)['data'] ?? null;
 
         if (!$anime) {
-            dd($request->all());
+            dd($request->mal_id);
             return to_route('home')
                 ->with('error', 'Anime não encontrado.');
         }
