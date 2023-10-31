0d1e44147a16b858f713f91e2e91d6b60feb3d73
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-10-31 00:22:15 -0300

    Update Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,7 @@
 import { Head } from "@inertiajs/vue3";
 
 import NewAuthLayout from "@/Layouts/NewAuthLayout.vue";
+import { Link } from "@inertiajs/vue3";
 
 import AnimeSavedFilterSelect from "@/Components/AnimeSavedFilterSelect.vue";
 import HomeFilterSelect from "@/Components/HomeFilterSelect.vue";
@@ -29,64 +30,72 @@ const props = defineProps({
                 Minha Lista
             </h2>
 
-            <div class="mt-6">
-                <div class="flex flex-col gap-3 sm:flex-row sm:gap-4">
-                    <HomeFilterSelect>
-                        <label
-                            for="orderBy"
-                            class="block w-24 text-sm font-medium min-w-fit text-white/50"
-                        >
-                            Pesquisar:
-                        </label>
+            <div v-if="$page.props.user">
+                <div class="mt-6">
+                    <div class="flex flex-col gap-3 sm:flex-row sm:gap-4">
+                        <HomeFilterSelect>
+                            <label
+                                for="orderBy"
+                                class="block w-24 text-sm font-medium min-w-fit text-white/50"
+                            >
+                                Pesquisar:
+                            </label>
 
-                        <input
-                            id="q"
-                            type="text"
-                            class="flex-1 block p-2 py-1 text-sm font-bold text-white bg-transparent border-none rounded outline-none appearance-none focus:ring-transparent disabled:text-white/30 disabled:placeholder:font-bold disabled:placeholder:text-white/30 sm:w-44"
-                            placeholder="Nome do Anime..."
-                            disabled
+                            <input
+                                id="q"
+                                type="text"
+                                class="flex-1 block p-2 py-1 text-sm font-bold text-white bg-transparent border-none rounded outline-none appearance-none focus:ring-transparent disabled:text-white/30 disabled:placeholder:font-bold disabled:placeholder:text-white/30 sm:w-44"
+                                placeholder="Nome do Anime..."
+                                disabled
+                            />
+                        </HomeFilterSelect>
+
+                        <HomeFilterSelect>
+                            <label
+                                for="orderBy"
+                                class="block w-24 text-sm font-medium min-w-fit text-white/50"
+                            >
+                                Ordenar por:
+                            </label>
+
+                            <AnimeSavedFilterSelect
+                                id="orderBy"
+                                :options="[{ label: 'Alfabético', value: '' }]"
+                                disabled
+                            />
+                        </HomeFilterSelect>
+
+                        <HomeFilterSelect>
+                            <label
+                                for="gender"
+                                class="block w-24 text-sm font-medium min-w-fit text-white/50"
+                            >
+                                Gêneros:
+                            </label>
+
+                            <AnimeSavedFilterSelect
+                                id="gender"
+                                :options="[{ label: 'Todos', value: '' }]"
+                                disabled
+                            />
+                        </HomeFilterSelect>
+                    </div>
+                </div>
+
+                <div class="max-w-full mt-6">
+                    <div class="flex gap-4 py-2 overflow-x-auto">
+                        <SavedAnimeCard
+                            v-for="anime in saved_animes"
+                            :anime="anime"
+                            :key="anime.id"
                         />
-                    </HomeFilterSelect>
-
-                    <HomeFilterSelect>
-                        <label
-                            for="orderBy"
-                            class="block w-24 text-sm font-medium min-w-fit text-white/50"
-                        >
-                            Ordenar por:
-                        </label>
-
-                        <AnimeSavedFilterSelect
-                            id="orderBy"
-                            :options="[{ label: 'Alfabético', value: '' }]"
-                            disabled
-                        />
-                    </HomeFilterSelect>
-
-                    <HomeFilterSelect>
-                        <label
-                            for="gender"
-                            class="block w-24 text-sm font-medium min-w-fit text-white/50"
-                        >
-                            Gêneros:
-                        </label>
-
-                        <AnimeSavedFilterSelect
-                            id="gender"
-                            :options="[{ label: 'Todos', value: '' }]"
-                            disabled
-                        />
-                    </HomeFilterSelect>
+                    </div>
                 </div>
             </div>
 
-            <div class="max-w-full mt-6">
-                <div class="flex gap-4 py-2 overflow-x-auto">
-                    <SavedAnimeCard
-                        v-for="anime in saved_animes"
-                        :anime="anime"
-                        :key="anime.id"
-                    />
+            <div v-else>
+                <div class="mt-6">
+                    <Link> Logue ou cadastre-se para ver seus animes </Link>
                 </div>
             </div>
         </div>
