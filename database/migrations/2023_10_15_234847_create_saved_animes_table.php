538cfdf47939bd20626c38b05f625f28cf666677
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-11-02 20:53:51 -0300

    Update 2023_10_15_234847_create_saved_animes_table.php

--- database/migrations/2023_10_15_234847_create_saved_animes_table.php
+++ database/migrations/2023_10_15_234847_create_saved_animes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('saved_animes', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(User::class);
 
             $table->bigInteger('mal_id');
             $table->string('title');
